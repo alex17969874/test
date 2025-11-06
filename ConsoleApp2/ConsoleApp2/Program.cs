5c71ddedb0942e8685bdd4eb2a8df49def32e82e
alex17969874 <aleks2007aleks10@gmail.com>
2025-11-06 10:58:55 +0200

    zadacha 1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter: ");
+        int n = int.Parse(Console.ReadLine());
+
+        n = Math.Abs(n);
+        int product = 1;
+        bool hasOdd = false;
+
+        foreach (char ch in n.ToString())
+        {
+            int digit = ch - '0';
+            if (digit % 2 != 0)
+            {
+                product *= digit;
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd)
+            Console.WriteLine(product);
+        else
+            Console.WriteLine(0);
+    }
+}
