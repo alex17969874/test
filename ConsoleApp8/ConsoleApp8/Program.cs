92364ef60ae728d1510c1786a9984ce72366eaa5
alex17969874 <aleks2007aleks10@gmail.com>
2025-11-11 15:49:40 +0200

    Create Program.cs

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿//Въведете положително цяло число от клавиатура и изведете на екрана всички негови
+//делители, които са по-малки от него на един ред, разделени с запетая и празно място след нея.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter positive number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Divisors: ");
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                if (i < number / 2 && number % (i + 1) == 0)
+                    Console.Write(i + ", ");
+                else
+                    Console.Write(i + (i == number - 1 ? "" : ", "));
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
