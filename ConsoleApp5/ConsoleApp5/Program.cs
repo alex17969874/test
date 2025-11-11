927658fecbe19c1ab08101f13a0dd942c733dd9c
alex17969874 <aleks2007aleks10@gmail.com>
2025-11-11 11:17:42 +0200

    Create Program.cs

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = int.Parse(Console.ReadLine());
+if (num < 0) 
+        
+    Console.WriteLine("Error");
+
+else
+{
+    double sum = 1;
+    if (num == 0)
+    {
+        Console.WriteLine(1);
+    }
+     else
+    {
+        for (int i = num; i > 0; i--)
+        {
+            sum = sum * 1;
+        }
+        Console.WriteLine(sum);
+    }
+}
